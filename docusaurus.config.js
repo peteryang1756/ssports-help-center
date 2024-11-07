eeb479ff469187d8b90e03de6edca1ce5607bfd3
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-08 07:12:06 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-   i18n: {
+  i18n: {
     defaultLocale: 'zh-tw',
     locales: ['zh-tw'],
   },
@@ -33,18 +33,13 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
         },
         blog: {
           showReadingTime: true,
-
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.         
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
-                },
+        },
       }),
     ],
   ],
@@ -57,107 +52,75 @@ const config = {
         logo: {
           alt: '雙龍體育 Logo',
           src: 'https://img.ssangyongsports.eu.org/logo2.png',
-          href: 'https://ssangyongsports.eu.org/support/,
-        className: 'custom-navbar-logo-class',
-             },
+          href: 'https://ssangyongsports.eu.org/support', // 修正結尾引號
+          className: 'custom-navbar-logo-class',
+        },
         items: [
-           {
+          {
             href: 'https://www.ssangyongsports.org/',
             label: '回首頁',
             position: 'left',
           },
         ],
       },
-              announcementBar: {
-      id: 'announcementBar-2',
-      content:
-        '新年快樂2023',
-      backgroundColor: '#84C1FF',
-      textColor: '#091E42',
-      isCloseable: false,
-    },
-          algolia: {
-      apiKey: "c2e792c2e75fe1dd3e40574f8b4c9a80",
-      indexName: "help",
-      appId: "70GEOCJCSX",
-      // Optional: see doc section bellow
-      contextualSearch: true,
-
-      // Optional: Algolia search parameters
-      searchParameters: {},
-
-      //... other Algolia params
-    },
-  
-  
+      announcementBar: {
+        id: 'announcementBar-2',
+        content: '新年快樂2023',
+        backgroundColor: '#84C1FF',
+        textColor: '#091E42',
+        isCloseable: false,
+      },
+      algolia: {
+        apiKey: "c2e792c2e75fe1dd3e40574f8b4c9a80",
+        indexName: "help",
+        appId: "70GEOCJCSX",
+        contextualSearch: true,
+        searchParameters: {},
+      },
       footer: {
         style: 'dark',
         logo: {
-        alt: '雙龍體育',
-        src: 'https://img.ssangyongsports.eu.org/logo2.png',
-        href: '/',
-        width: 160,
-        height: 51,
-      },
+          alt: '雙龍體育',
+          src: 'https://img.ssangyongsports.eu.org/logo2.png',
+          href: '/',
+          width: 160,
+          height: 51,
+        },
         links: [
           {
-          title: '體育',
+            title: '體育',
             items: [
-              {
-                  label: '雙龍職棒',
-                  href: 'https://slbtw.cf',
-              },
-              {
-                label: '雙龍職籃',
-                href: 'https://sbatw.ml',
-              },
-              {
-                label: '雙龍足球',
-                href: '/',
-              },
+              { label: '雙龍職棒', href: 'https://slbtw.cf' },
+              { label: '雙龍職籃', href: 'https://sbatw.ml' },
+              { label: '雙龍足球', href: '/' },
             ],
           },
           {
-           title: '組織',
+            title: '組織',
             items: [
-              {
-                label: 'blog',
-                href: '/blog',
-              },
-              {
-                label: '狀態',
-                href: 'https://status.ssangyongsports.eu.org/',
-              },              
+              { label: 'blog', href: '/blog' },
+              { label: '狀態', href: 'https://status.ssangyongsports.eu.org/' },
             ],
           },
           {
             title: '幫助',
             items: [
-              {
-                label: '幫助',
-                href: 'https://help.ssangyongsports.eu.org/',
-              },
-              {
-                label: '社區',
-                href: 'https://discuss.ssangyongsports.eu.org/',
-              },
-              {
-                label: '聯繫',
-            href: 'https://ssangyongsports.eu.org/c',
-              },
+              { label: '幫助', href: 'https://help.ssangyongsports.eu.org/' },
+              { label: '社區', href: 'https://discuss.ssangyongsports.eu.org/' },
+              { label: '聯繫', href: 'https://ssangyongsports.eu.org/c' },
             ],
           },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} 雙龍體育, org. 維護團隊：雙龍體育.`,
       },
-       prism: {
+      prism: {
         theme: lightCodeTheme,
-        darkTheme: lightCodeTheme
+        darkTheme: lightCodeTheme,
       },
       colorMode: {
         defaultMode: 'light',
         disableSwitch: true,
-        respectPrefersColorScheme: false
+        respectPrefersColorScheme: false,
       },
     }),
 };
