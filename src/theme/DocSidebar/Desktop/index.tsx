51bb6781515d81b6d248144d0a94c2eae3bc0047
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-09 21:51:29 +0800

    Update index.tsx

--- src/theme/DocSidebar/Desktop/index.tsx
+++ src/theme/DocSidebar/Desktop/index.tsx
@@ -1,38 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import clsx from 'clsx';
+import React from 'react';
 import {useThemeConfig} from '@docusaurus/theme-common';
-import Logo from '@theme/Logo';
-import Content from '@theme/DocSidebar/Desktop/Content';
 import type {Props} from '@theme/DocSidebar/Desktop';
 
 function DocSidebarDesktop({path, sidebar, onCollapse, isHidden}: Props) {
-  const {
-    navbar: {hideOnScroll},
-    docs: {
-      sidebar: {hideable = true}, // Make hideable default to true
-    },
-  } = useThemeConfig();
-
-  const [hidden, setHidden] = useState(true); // Set initial state to hidden
-
-  // Call onCollapse to sync with parent state if necessary
-  useEffect(() => {
-    if (hidden && onCollapse) {
-      onCollapse();
-    }
-  }, [hidden, onCollapse]);
-
-  return (
-    <div
-      className={clsx(
-        'sidebar', // Plain string class names
-        hideOnScroll && 'sidebarWithHideableNavbar',
-        hidden && 'sidebarHidden', // Apply 'sidebarHidden' when hidden
-      )}>
-      {hideOnScroll && <Logo tabIndex={-1} className="sidebarLogo" />}
-      <Content path={path} sidebar={sidebar} />
-    </div>
-  );
+  // Sidebar is completely removed
+  return null;
 }
 
 export default React.memo(DocSidebarDesktop);
