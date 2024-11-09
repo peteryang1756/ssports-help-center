b5624a3d16f0a5d2d914338b8f4f91273c3714f4
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-09 19:52:32 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,8 @@
 module.exports = {
   docs: [
     {
-      type: 'category',
-      label: 'Your Category',
       collapsible: true,
       collapsed: true, // This will make it hidden by default
-      items: ['doc1', 'doc2'],
     },
   ],
 };
