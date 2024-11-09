0dd8fb6d3244fb936061ba55dfef9a8bbc6af1a8
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-09 19:51:31 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,12 +28,20 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
         blog: {
           showReadingTime: true,
+
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.         
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
-        },
+                },
       }),
     ],
   ],
