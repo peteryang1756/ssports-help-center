9b8c4b65cb9f9cd12f1a776a45e19cbadd691a4c
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-11 20:12:37 +0800

    Update Footer.js

--- src/theme/Footer.js
+++ src/theme/Footer.js
@@ -9,11 +9,11 @@ export default function FooterWrapper(props) {
   } = useDocusaurusContext()
   const icon = <img src="https://88ij.vercel.app/1000001111-removebg-preview.png?text=Logo" />
   const popupText = "詢問任何問題?"
-
+  const welcomeMessage = "任何需要幫雙龍體育協助嗎?"
 
   return (
     <>
-      <MendableFloatingButton icon={icon} popupText={popupText} anon_key='d24adf46-bada-44ef-a25b-94d01fe6fb3e' />
+      <MendableFloatingButton icon={icon} popupText={popupText} welcomeMessage={welcomeMessage} anon_key='d24adf46-bada-44ef-a25b-94d01fe6fb3e' />
       <Footer {...props} />
     </>
   )
