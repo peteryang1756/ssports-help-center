c7a812fd6fef93224b5f9900bb7d6cf8f9b2966e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-11 20:15:29 +0800

    Update Footer.js

--- src/theme/Footer.js
+++ src/theme/Footer.js
@@ -10,10 +10,13 @@ export default function FooterWrapper(props) {
   const icon = <img src="https://88ij.vercel.app/1000001111-removebg-preview.png?text=Logo" />
   const popupText = "詢問任何問題?"
   const welcomeMessage = "任何需要幫雙龍體育協助嗎?"
-
+const floatingButtonStyle = {
+    color: "#e2e8f0",
+    backgroundColor: "#e2e8f0"
+}
   return (
     <>
-      <MendableFloatingButton icon={icon} popupText={popupText} welcomeMessage={welcomeMessage} anon_key='d24adf46-bada-44ef-a25b-94d01fe6fb3e' />
+      <MendableFloatingButton icon={icon} popupText={popupText} welcomeMessage={welcomeMessage} floatingButtonStyle={floatingButtonStyle} anon_key='d24adf46-bada-44ef-a25b-94d01fe6fb3e' />
       <Footer {...props} />
     </>
   )
