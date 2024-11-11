6dce7e8433d928f5ab722207cc21fb24778fbc01
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-11 20:27:28 +0800

    Update Footer.js

--- src/theme/Footer.js
+++ src/theme/Footer.js
@@ -16,7 +16,7 @@ const floatingButtonStyle = {
 }
   return (
     <>
-      <MendableFloatingButton icon={icon} popupText={popupText} welcomeMessage={welcomeMessage} floatingButtonStyle={floatingButtonStyle} anon_key='d24adf46-bada-44ef-a25b-94d01fe6fb3e' />
+      <MendableFloatingButton icon={icon} popupText={popupText} welcomeMessage={welcomeMessage} floatingButtonStyle={floatingButtonStyle} anon_key='d24adf46-bada-44ef-a25b-94d01fe6fb3e' dialogPlaceholder="如何聯繫雙龍體育?" />
       <Footer {...props} />
     </>
   )
