0287aa77b5f32b64b9afbe545dd06f9696462d1b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-11 23:27:18 +0800

    Update Footer.js

--- src/theme/Footer.js
+++ src/theme/Footer.js
@@ -27,7 +27,7 @@ export default function FooterWrapper(props) {
     <>
       <MendableFloatingButton
         icon={icon}
-        dismissPopupAfter={2}
+        dismissPopupAfter={0.000001}
         welcomeMessage={welcomeMessage}
         messageSettings={{ hideSources: true }} // Fixed object syntax
         floatingButtonStyle={floatingButtonStyle}
