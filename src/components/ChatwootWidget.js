12487864997a855f5ca96b360af68628baae484b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-11-12 09:06:10 +0800

    Update ChatwootWidget.js

--- src/components/ChatwootWidget.js
+++ src/components/ChatwootWidget.js
@@ -2,24 +2,32 @@ import React, { useEffect } from 'react';
 
 const ChatwootWidget = () => {
   useEffect(() => {
-    (function(d, t) {
-      const g = d.createElement(t);
-      const s = d.getElementsByTagName(t)[0];
-      g.src = "https://ssportlivechat.onrender.com/embed.js";
-      s.parentNode.insertBefore(g, s);
-      g.onload = function() {
-        new window.ChaskiqMessengerEncrypted({
-          domain: 'https://ssportlivechat.onrender.com',
-          ws: 'wss://ssportlivechat.onrender.com/cable',
-          app_id: "egXA8jUz5tjZq5UE3xUkH7mH",
-          data: "YOUR_ENCRYPTED_JWE_DATA",
-          lang: "USER_LANG_OR_DEFAULTS_TO_BROWSER_LANG"
-        });
-      };
-    })(document, "script");
+    // Define the Beacon function and load the script
+    (function (e, t, n) {
+      function a() {
+        var e = t.getElementsByTagName('script')[0],
+          n = t.createElement('script');
+        n.type = 'text/javascript';
+        n.async = true;
+        n.src = 'https://beacon-v2.helpscout.net';
+        e.parentNode.insertBefore(n, e);
+      }
+      if (
+        (e.Beacon = n = function (t, n, a) {
+          e.Beacon.readyQueue.push({ method: t, options: n, data: a });
+        }),
+        (n.readyQueue = []),
+        'complete' === t.readyState
+      )
+        return a();
+      e.attachEvent ? e.attachEvent('onload', a) : e.addEventListener('load', a, false);
+    })(window, document, window.Beacon || function () {});
+
+    // Initialize the Beacon
+    window.Beacon('init', '8f33fda0-c237-4265-9f64-9e54009c53ce');
   }, []);
 
-  return null;
+  return null; // This component doesn't render anything to the UI
 };
 
 export default ChatwootWidget;
