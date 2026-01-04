c6d077c458cbffed9d3f68d5e69824b29babd665
楊Peter <92738287+peteryang1756@users.noreply.github.com>
2026-01-05 00:05:08 +0800

    Update ChatwootWidget.js

--- src/components/ChatwootWidget.js
+++ src/components/ChatwootWidget.js
@@ -2,29 +2,22 @@ import React, { useEffect } from 'react';
 
 const ChatwootWidget = () => {
   useEffect(() => {
-    // Define the Beacon function and load the script
-    (function (e, t, n) {
-      function a() {
-        var e = t.getElementsByTagName('script')[0],
-          n = t.createElement('script');
-        n.type = 'text/javascript';
-        n.async = true;
-        n.src = 'https://beacon-v2.helpscout.net';
-        e.parentNode.insertBefore(n, e);
-      }
-      if (
-        (e.Beacon = n = function (t, n, a) {
-          e.Beacon.readyQueue.push({ method: t, options: n, data: a });
-        }),
-        (n.readyQueue = []),
-        'complete' === t.readyState
-      )
-        return a();
-      e.attachEvent ? e.attachEvent('onload', a) : e.addEventListener('load', a, false);
-    })(window, document, window.Beacon || function () {});
-
-    // Initialize the Beacon
-    window.Beacon('init', '8f33fda0-c237-4265-9f64-9e54009c53ce');
+    function loadCCWidget() {
+      (new window["click-connector-widget"]).mount({});
+    }
+    function loadCCScript() {
+      var t = document.createElement("script");
+      t.id = "cc-widget-script";
+      t.setAttribute("data-widget-id", "piu03u-4ljgo");
+      t.type = "text/javascript";
+      t.defer = true;
+      t.addEventListener("load", function(t) {
+        loadCCWidget();
+      });
+      t.src = "https://widget.clickconnector.app/widget.js";
+      document.getElementsByTagName("head")[0].appendChild(t);
+    }
+    loadCCScript();
   }, []);
 
   return null; // This component doesn't render anything to the UI
